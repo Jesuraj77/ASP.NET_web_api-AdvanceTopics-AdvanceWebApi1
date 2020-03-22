5a3f439bb0d965105097e4f9a1174da393ad255b
Jesuraj77 <isysraju2001@gmail.com>
2020-03-22 13:51:06 +0530

    Added

---
+++ AdvanceWebApi1/Controllers/DependencyController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceWebApi1.Controllers
+{
+    public class DependencyController : Controller
+    {
+        //code here to implement Dependency injection.
+        //Show how the ASP.NET core framework supports its default container and register the services using various options.
+    }
+}
